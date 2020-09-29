67c5ddd44c968c85c1fab761b16838606b226b8f
michalkrol-pro <62615562+michalkrol-pro@users.noreply.github.com>
2020-09-29 16:47:02 +0200

    Initial commit

---
+++ src/pasja/mimo/Main.java
@@ -0,0 +1,9 @@
+package pasja.mimo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("To jest Projekt realizowany wraz z PasjaInformatyki- OBIEKTOWOŚĆ");
+
+    }
+}
---
+++ src/pasja/mimo/Odcinek1.java
@@ -0,0 +1,7 @@
+package pasja.mimo;
+
+public class Odcinek1 {
+    public static void main(String[] args) {
+        System.out.println("W tym odcinku robimy zadanie ");
+    }
+}
